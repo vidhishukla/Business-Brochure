63c06b3d00c40a34a2605f9daddbd0550b4e5151
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-07-17 22:42:34 -0700

    Basic Nav function for movile added
    Basic Nav function for movile added/fontawsome icons on footer added

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,5 @@ const navButton = document.getElementById('nav-button');
 
 const nav = document.getElementById('nav');
 
-//function to show and hide nav
-// navButton.addEventListener('click', _ => nav.classList.toggle('active'));
+// function to show and hide nav
+navButton.addEventListener('click', _ => nav.classList.toggle('active'));
