fcfa71d4717b3e670b7e6155501343ac93e82007
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-07-21 15:10:25 -0700

    Revert "Merge branch 'master' into Steven-branch"
    This reverts commit cdc0dbb922feb7ae721f1f67f8da90e16308ff77, reversing
    changes made to 523f14b0802a15e1255f1f81e1ff15530ed97f1c.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,4 @@
 // scripts.js
-// $(function () {
-//   alert("hello");
-// });
-
-const navButton = document.getElementById("nav-button");
-
-const nav = document.getElementById("nav");
-
-// function to show and hide nav
-navButton.addEventListener("click", (_) => nav.classList.toggle("active"));
-
-const $btnPrevNext = $("#prev, #next");
-$btnPrevNext.mouseDown(function (e) {
-  e.preventDefault();
+$(function () {
+  alert("hello");
 });
